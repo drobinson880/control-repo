ef5be848b3b310acd8fa6a2399ac95833bdff16d
Dan Robinson <dan.robinson@dan.robinson-C02MQ15KFD57>
2018-10-30 12:29:58 -0400

    eyaml test

---
+++ site/eyaml_test/manifests/test_class.pp
@@ -0,0 +1,11 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include eyaml_test::test_class
+class eyaml_test::test_class {
+
+  notify {"$test_string":}
+
+}
